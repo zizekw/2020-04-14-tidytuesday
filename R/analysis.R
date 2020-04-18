7d3a0fe5893222a0912a6a4c2d646820c1161f1b
Bill Zizek <william.zizek@gmail.com>
2020-04-18 14:40:56 -0400

    got the data

---
+++ R/analysis.R
@@ -0,0 +1,12 @@
+# initialize
+
+library(tidyverse)
+
+# Get the Data
+
+polls <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-14/polls.csv')
+rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-14/rankings.csv')
+
+# write_csv(polls, "./data/polls.csv")
+# write_csv(rankings, "./data/rankings.csv")
+
